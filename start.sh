9e70e49035b1ae883f2d3ad73a31ede1aace6b14
flymyd <2663797538@qq.com>
2024-02-12 04:30:44 +0800

    update

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ chmod +x /root/noVNC/utils/websockify/run
 Xvfb :1 -screen 0 ${VNC_RESOLUTION}x${VNC_COL_DEPTH} &
 x11vnc -display :1 -passwd ${VNC_PASSWORD} -xkb -forever -shared -rfbport ${VNC_PORT} &
 /root/noVNC/utils/novnc_proxy --vnc 0.0.0.0:${VNC_PORT} --listen ${NOVNC_PORT} &
-# qq --no-sandbox &
+qq --no-sandbox &
 tail -f /dev/null
